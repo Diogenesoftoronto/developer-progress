67d57ee8bd50f787c6bb1d9b4b2009cf18a764cd
Diogenesoftoronto <kmannoel@gmail.com>
2022-03-12 00:51:06 -0500

    sort of works but also doesnt lol

--- MaxConsecutiveOnes/maxConsecutiveOnes.js
+++ MaxConsecutiveOnes/maxConsecutiveOnes.js
@@ -1,42 +1,61 @@
 const findMaxConsecutiveOnes = function (nums) {
+  // define the left and right of the array
+  let left = 0;
+  let right = nums.length;
+  let result = right;
+  const cpNums = [...nums];
+  // console.table(cpNums)
+  //   trying out the recursive solution
   // first create the variables to store the data
-  if (!nums[0]) return null;
+  if (nums.length === 0) return null;
 
-  const obj = { prevCountOnes: 0, currCountOnes: 0, result: 0 };
-  nums.forEach((element, index) => {
-    if (!element) {
-      obj.currCountOnes = index;
-      if (obj.currCountOnes > obj.prevCountOnes) {
-        obj.result = obj.currCountOnes - obj.prevCountOnes;
+//  call the recursive solution
+  for(let index = 0; index < cpNums.length; index++) {
+    // check if element is 0
+    if (cpNums[index] === 0) {
+      left = index;
+      right = (cpNums.length - 1) - left;
+      result = right;
+
+      // check if left side is greater than right side
+      if (left > right) {
+        result = left;
+        console.table({ right, left, index, result, cpNums: cpNums.length });
+        console.table(cpNums)
+        return result;
       }
-      obj.prevCountOnes = obj.currCountOnes - obj.prevCountOnes;
-      // obj.longest = obj.currCount;
-    }
-  });
-  //  subtract the end of the array from the current count of ones and set result to that number if greater than result
-  if (obj.currCountOnes === 0) {
-    obj.result = nums.length
-  }
-  const someVar = (nums.length - 1) - obj.currCountOnes;
-  console.log('someVar', someVar);
-  if (someVar > obj.result) {
-    obj.result = someVar;
-  }
+      // pop the left side of the array off the stack
+      cpNums.splice(0, index + 1);
 
-  console.log(nums)
-  console.table(obj)
-  // hey
-  return obj.result;
+    }
+  };
+
+  // console.table({ right, left, result, cpNums: cpNums.length });
+
+  return result;
 };
+
+function recursiveMaxConsecutiveOnes(nums) {
+  //  define the base case: the base will be an array of ones 
+  
+
+}
+
 // using consoles immense power for good by creating a test tool with just console
 const testArrs = [
   { var: [], result: null },
   { var: [1, 0], result: 1 },
   { var: [1, 0, 1], result: 1 },
-  { var: [1, 0, 1, 1, 1, 0, 1], result: 3 },
+  { var: [1, 0, 1, 1, 1, 0, 1, 1], result: 3 },
   { var: [1, 1, 0, 1, 1, 1], result: 3 },
-  { var: [1, 2, 0, 1, 1], result: null },
-  { var: [1, 1, 1], result: 3 },
+  { var: [1, 1, 1, 0, 1, 1], result: 3 },
+  { var: [1, 1, 1, 1], result: 3 },
+  { var: [0, 0], result: 0 },
+  { var: [0, 1], result: 1 },
+  { var: [0, 0, 1, 1], result: 1 },
+  { var: [0, 0, 1, 1, 0, 0], result: 1 },
+  { var: [0, 0, 0, 0], result: 1 },
+
 ];
 var testFunc = function (func, testVars, funcName) {
   // conditional if tests pass
@@ -54,7 +73,7 @@ var testFunc = function (func, testVars, funcName) {
           results: `These tests did not pass: ${resultArrBool.reduce(
             (acc, ele, index) => {
               if (ele === false) {
-                acc.push(index + 1);
+                acc.push(index);
               }
               return acc;
             },
@@ -69,9 +88,13 @@ var testFunc = function (func, testVars, funcName) {
   // let expectation = true;
   const benchmarkFunc = (func, testVars) => {
     testVars.forEach((test, index) => {
-      console.time(`benchmark for ${funcName} and test #${index}`);
+      console.time(
+        `benchmark for ${funcName} and test #${index} and the answer is ${test.result}`
+      );
       func(test.var);
-      console.timeEnd(`benchmark for ${funcName} and test #${index}`);
+      console.timeEnd(
+        `benchmark for ${funcName} and test #${index} and the answer is ${test.result}`
+      );
     });
   };
   benchmarkFunc(func, testVars);
