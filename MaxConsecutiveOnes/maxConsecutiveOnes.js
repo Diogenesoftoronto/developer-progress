4001bf307bcfb701eac795ea1eeba8ab863f663c
Diogenesoftoronto <kmannoel@gmail.com>
2022-03-13 00:47:16 -0500

    add starting variables to the recursivefunction

--- MaxConsecutiveOnes/maxConsecutiveOnes.js
+++ MaxConsecutiveOnes/maxConsecutiveOnes.js
@@ -37,7 +37,10 @@ const findMaxConsecutiveOnes = function (nums) {
 
 function recursiveMaxConsecutiveOnes(nums) {
   //  define the base case: the base will be an array of ones 
-  
+  let left = 0;
+  let right = nums.length;
+  let result = right;
+  const cpNums = [...nums];
 
 }
 
