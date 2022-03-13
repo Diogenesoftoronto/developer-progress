da85644b2947f39ae5786a3f9c310d64362a7a1a
Diogenesoftoronto <kmannoel@gmail.com>
2022-03-13 03:38:35 -0400

    changes

--- MaxConsecutiveOnes/maxConsecutiveOnes.js
+++ MaxConsecutiveOnes/maxConsecutiveOnes.js
@@ -1,64 +1,55 @@
 const findMaxConsecutiveOnes = function (nums) {
   // define the left and right of the array
-  let left = 0;
-  let right = nums.length;
-  let result = right;
-  const cpNums = [...nums];
-  // console.table(cpNums)
-  //   trying out the recursive solution
-  // first create the variables to store the data
-  if (nums.length === 0) return null;
-
-//  call the recursive solution
-  for(let index = 0; index < cpNums.length; index++) {
+  let max = 0;
+  let prev = 0;
+  let current = 0;
+  // if (nums.length === 0) return null;
+  console.table(nums)
+  
+  for (let index = 0; index < nums.length; index++) {
+    current++; 
     // check if element is 0
-    if (cpNums[index] === 0) {
-      left = index;
-      right = (cpNums.length - 1) - left;
-      result = right;
-
+    if (!nums[index]) {
+      // consider midpoints and that i need to return a count
       // check if left side is greater than right side
-      if (left > right) {
-        result = left;
-        console.table({ right, left, index, result, cpNums: cpNums.length });
-        console.table(cpNums)
-        return result;
+      // need to return a count and not an index
+      console.table({index, max, prev, nums: nums.length, console: 1, current });
+      
+      if (current > max) {
+        max = current;
+        console.table({index, max, prev, nums: nums.length , console: 2});
       }
-      // pop the left side of the array off the stack
-      cpNums.splice(0, index + 1);
-
+      prev = current;
+      current = 0;
     }
-  };
+  }
 
   // console.table({ right, left, result, cpNums: cpNums.length });
-
-  return result;
+  return max;
 };
 
 function recursiveMaxConsecutiveOnes(nums) {
-  //  define the base case: the base will be an array of ones 
+  //  define the base case: the base will be an array of ones
   let left = 0;
   let right = nums.length;
   let result = right;
   const cpNums = [...nums];
-
 }
 
 // using consoles immense power for good by creating a test tool with just console
 const testArrs = [
-  { var: [], result: null },
+  // { var: [], result: null },
   { var: [1, 0], result: 1 },
   { var: [1, 0, 1], result: 1 },
   { var: [1, 0, 1, 1, 1, 0, 1, 1], result: 3 },
-  { var: [1, 1, 0, 1, 1, 1], result: 3 },
-  { var: [1, 1, 1, 0, 1, 1], result: 3 },
-  { var: [1, 1, 1, 1], result: 3 },
+  // { var: [1, 1, 0, 1, 1, 1], result: 3 },
+  // { var: [1, 1, 1, 0, 1, 1], result: 3 },
+  { var: [1, 1, 1, 1], result: 4 },
   { var: [0, 0], result: 0 },
   { var: [0, 1], result: 1 },
-  { var: [0, 0, 1, 1], result: 1 },
-  { var: [0, 0, 1, 1, 0, 0], result: 1 },
-  { var: [0, 0, 0, 0], result: 1 },
-
+  // { var: [0, 0, 1, 1], result: 2 },
+  // { var: [0, 0, 1, 1, 0], result: 2 },
+  // { var: [0, 0, 1], result: 1 },
 ];
 var testFunc = function (func, testVars, funcName) {
   // conditional if tests pass
@@ -91,12 +82,13 @@ var testFunc = function (func, testVars, funcName) {
   // let expectation = true;
   const benchmarkFunc = (func, testVars) => {
     testVars.forEach((test, index) => {
+      funcResult = func(test.var);
       console.time(
-        `benchmark for ${funcName} and test #${index} and the answer is ${test.result}`
+        `benchmark for ${funcName} test #${index}: answer is ${test.result}, result is ${funcResult}`
       );
       func(test.var);
       console.timeEnd(
-        `benchmark for ${funcName} and test #${index} and the answer is ${test.result}`
+        `benchmark for ${funcName} test #${index}: answer is ${test.result}, result is ${funcResult}`
       );
     });
   };
