b376b48f428340e2475c4bd2f33ded9ac6cc23da
Diogenesoftoronto <kmannoel@gmail.com>
2022-03-13 00:47:27 -0500

    seperate the file

---
+++ MaxConsecutiveOnes/recursiveMaxConsecutiveOnes.js
@@ -0,0 +1,8 @@
+function recursiveMaxConsecutiveOnes(nums) {
+  //  define the base case: the base will be an array of ones 
+  let left = 0;
+  let right = nums.length;
+  let result = right;
+  const cpNums = [...nums];
+
+}
