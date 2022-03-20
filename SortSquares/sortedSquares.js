d61684ff33307fed23407131be61098f322117e6
Diogenesoftoronto <kmannoel@gmail.com>
2022-03-19 23:31:55 -0400

    js solution

---
+++ SortSquares/sortedSquares.js
@@ -0,0 +1,8 @@
+const sortedSquares = function(nums) {
+  return nums.map((e)=>e*e).sort((a,b)=>a-b);
+};
+
+const sortSquares = (nums) => {
+  sortedSquaresNums.map((e)=> element**2)
+return sortedSquaresNums
+}
