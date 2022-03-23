059bc8877a5fba896aacb854a2a240b833345d4f
Diogenesoftoronto <kmannoel@gmail.com>
2022-03-23 17:16:55 -0400

    add binary function

---
+++ AddBinaries/addBinary.js
@@ -0,0 +1,14 @@
+var addBinary = function(a, b) {
+  const sum = (a, b) => {
+      let storeBit;
+      const sumArray = a.reduce((acc, element, index)=> {
+          let elementfromA = element;
+          let elementfromB = b.charAt(index);
+          (elementfromA && !elementfromB) || (!elementfromA && elementfromB) || (elementfrom)? acc.push(parseInt(elementfromA)) : 
+          elementfromA && elementfromB ? acc.push(0) : 
+      }, []) 
+     // const arrA = a.split('')
+     // const arrB = b.split('')
+      // parseInt(a) > parseInt(b) ? 
+  }
+};
